545803e952497e68f60b574a39b983bfb93a705d
shravniks <shravani.nikam22@spit.ac.in>
2025-02-05 17:08:46 +0530

    3rd change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,13 +94,19 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD":os.getenv("DB_PWD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
+
+
+# Password validat
 
 
 # Password validation
